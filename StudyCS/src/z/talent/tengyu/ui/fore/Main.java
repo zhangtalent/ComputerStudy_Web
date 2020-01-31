538a8fc7da142b254bfb1221039e48ec983eb98a
zhangtalent <1322293658@qq.com>
2020-01-31 10:52:49 +0800

    ff

--- StudyCS/src/z/talent/tengyu/ui/fore/Main.java
+++ StudyCS/src/z/talent/tengyu/ui/fore/Main.java
@@ -1,4 +1,6 @@
 package z.talent.tengyu.ui.fore;
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 /**
  * @author zhangtalent
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import z.talent.tengyu.bean.Journal;
 import z.talent.tengyu.mapper.JournalMapper;
 
 @Controller
@@ -22,8 +25,16 @@ public class Main {
 	@Autowired
 	private JournalMapper journalMapper;
 	@GetMapping("/")
-    public String ProfileMain(Model model) {
-		model.addAttribute("datas", journalMapper.getJournals(0, 10));
+    public String ProfileMain(Model model,@RequestParam(value = "page",required = false)Integer page) {
+		
+		
+		int counts = journalMapper.getJournalCounts();
+		page = page==null?0:page;
+		int offset = page*10;
+		ArrayList<Journal> arg1 = journalMapper.getJournals(offset, 10);
+		model.addAttribute("datas", arg1);
+		model.addAttribute("next", offset+10>counts?-1:page+1);
+		model.addAttribute("previous", page==0?-1:page-1);
 		return "index";
     }
 	
